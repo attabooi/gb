c6e997174bbe4c0716a37594df9ed2bf20f02492
Joon <joonqpa@gamil.com>
2023-07-01 21:25:27 +0900

    modify

--- api.js
+++ api.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use('/css', express.static(path.join(__dirname, 'Vintage/css')));
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
