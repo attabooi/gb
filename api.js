119f011d4131c3716d486902510ce2f550624ee3
Joon <joonqpa@gamil.com>
2023-07-01 19:31:58 +0900

    modify api.js

--- api.js
+++ api.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Replace with your desired port number
+const port = 3000;
 
+// Serve static files from the current directory
+app.use(express.static(__gb));
+
+// Route for the home page
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(__gb + '/index.html');
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-
